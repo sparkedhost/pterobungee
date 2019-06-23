1a17e51cce06e34aa38a2de73fcf20b24661070a
Raúl <between13131@gmail.com>
2019-06-23 19:15:53 +0200

    Initial commit

---
+++ src/me/bettd/pterobungee/Main.java
@@ -0,0 +1,36 @@
+package me.bettd.pterobungee;
+
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.command.ConsoleCommandSender;
+
+import java.util.logging.Level;
+
+public class Main extends Plugin {
+    @Override
+    public void onEnable() {
+        getProxy().getPluginManager().registerCommand(this, new StopCmd());
+        getLogger().log(Level.INFO, "PteroBungee loaded. v" + getProxy().getPluginManager().getPlugin("PteroBungee").getDescription().getVersion() + " developed by BetTD");
+        Runnable doneOutput = new Runnable() {
+            public void run() {
+                try {
+                    sendStartedMsg();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(doneOutput).start();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "PteroBungee disabled. Have a great day.");
+    }
+
+    public void sendStartedMsg() throws InterruptedException {
+        /*Thread.sleep(5000);*/
+        ConsoleCommandSender console = (ConsoleCommandSender) getProxy().getConsole();
+        console.sendMessage(new TextComponent("Done (2,100s)! For help, type \"help\" or \"?\""));
+    }
+}
